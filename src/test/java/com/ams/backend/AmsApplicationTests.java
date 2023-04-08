0a73c919b9da6916e51c283659e4f90f9a4ce96a
marianrl <82778834+marianrl@users.noreply.github.com>
2023-04-08 11:14:28 -0300

    Creating repo

---
+++ src/test/java/com/ams/backend/AmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
