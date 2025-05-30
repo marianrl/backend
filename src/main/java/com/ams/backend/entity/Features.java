5ad6c62b177a401642e647c6a52fd492604aae2c
marianrl <marianoh.rodriguez@davinci.edu.ar>
2025-05-29 21:35:10 -0300

    Borramos import innecesario

--- src/main/java/com/ams/backend/entity/Features.java
+++ src/main/java/com/ams/backend/entity/Features.java
@@ -1,6 +1,5 @@
 package com.ams.backend.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
